f7b2084e76bd2abacd49efe0a1a93dd751e786ee
Gerry Migwi <migwi.dev@gmail.com>
2019-04-05 17:09:10 +0000

    Created and scripted player animations.

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	private Rigidbody2D _playerRigidbody;
+	private PlayerAnimation _playerAnimation;
+	private SpriteRenderer _playerSpriteRenderer;
+	private bool _isPlayerflipped = false;
 	private float _jumpForce = 5.0f;
 	[SerializeField]
 	private float _playerSpeed = 3.0f;
@@ -12,11 +15,14 @@ public class Player : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		_playerRigidbody = GetComponent<Rigidbody2D>();
+		_playerAnimation = GetComponent<PlayerAnimation>();
+		_playerSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Movement();
+		GroundAttack();
 	}
 
 	// Handle movement of the player
@@ -24,16 +30,36 @@ public class Player : MonoBehaviour {
 		// Handle vertical movement of player
 		float move = Input.GetAxisRaw("Horizontal");
 		_playerRigidbody.velocity = new Vector2(move * _playerSpeed, _playerRigidbody.velocity.y);
+		if(move == 1) {
+			_isPlayerflipped = false;
+		} else if(move == -1) {
+			_isPlayerflipped = true;
+		}
+		_playerSpriteRenderer.flipX = _isPlayerflipped;
+		_playerAnimation.Run(move);
 
 		// Draw raycast below player
 		Debug.DrawRay(transform.position, Vector2.down * 1.0f, Color.blue);
 
 		// Handle jumping of player
-		if(Input.GetKeyDown(KeyCode.Space)) {
+		_playerAnimation.Jump(false);
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse1)) {
 			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1 << 8);
 			if(hit.collider != null) {
 				_playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x, _jumpForce);
+				if(Input.GetKeyDown(KeyCode.Space)) {
+					_playerAnimation.Jump(true);
+				} else if(Input.GetKeyDown(KeyCode.Mouse1)) {
+					_playerAnimation.JumpAttack();
+				}
 			}
+		}
+	}
+
+	// Handle player attack functionality
+	void GroundAttack() {
+		if(Input.GetKeyDown(KeyCode.Mouse0)) {
+			_playerAnimation.GroundAttack();
 		}
 	}
 }
