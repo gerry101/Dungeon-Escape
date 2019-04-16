0ddaad0915f1c432119528498d2ef18c60970c54
Gerry Migwi <migwi.dev@gmail.com>
2019-04-16 09:23:33 +0000

    Added the shop UI.

---
+++ Assets/Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _shopPanel;
+
+	// Open shop if player collides with shopkeeper
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player") {
+			Player player = other.GetComponent<Player>();
+			if(player != null) {
+				UIManager.Instance.OpenShop(player.gems);
+			}
+			_shopPanel.SetActive(true);
+		}
+	}
+
+	// Close shop if player leaves shopkeeper
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.tag == "Player") {
+			_shopPanel.SetActive(false);
+		}
+	} 
+}
---
+++ Assets/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	private static UIManager _instance;
+	public static UIManager Instance {
+		get{
+			if(_instance == null) {
+				Debug.LogError("UIManager is NULL");
+			}
+
+			return _instance;
+		}
+	}
+
+	public Text playerGemCount;
+
+	public void OpenShop(int gemCount) {
+		string updatedGemCount = gemCount.ToString() + "G";
+		playerGemCount.text = updatedGemCount;
+	}
+
+	void Awake() {
+		_instance = this;
+	}
+}
